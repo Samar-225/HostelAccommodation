8907526ed5e968ee172a9dfba3c9f7a5ec690038
amshinde941 <amshinde941@gmail.com>
2021-03-17 23:00:06 +0530

    commit

---
+++ react/hello-world/src/components/pages/Hostel.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Hostel() {
+  return (
+    <>
+      <h1 className='hostel'>Hostel</h1>
+    </>
+  );
+}
---
+++ react/hello-world/src/components/pages/Mess.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Mess() {
+  return (
+    <>
+      <h1 className='mess'>This is under development.</h1>
+    </>
+  );
+}
